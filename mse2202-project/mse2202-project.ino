c4f29fbc30775778d9ca770b50f1ba751e22e7c9
Aarish Adeel <aarish8@gmail.com>
2021-03-21 16:43:00 -0400

    Add DC motor and start/stop functionality with push button 1

--- mse2202-project/mse2202-project.ino
+++ mse2202-project/mse2202-project.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+/* Sensor Pins */
+const int buttonPin = 27;
 
+/* Motor Pins */
+const int climbMotorFPin = 15;
+const int climbMotorRPin = 2;
+
+/* Sensor State/Time Variables */
+int button = 1;
+int buttonLast = 1;
+
+/* Motor State/Time Variables */
+bool motorOn = false;
+
+void setup() {
+   // Sensor pin setup
+   pinMode(buttonPin, INPUT_PULLUP);
+  
+   // ledc (LED Control) pins setup, used as PWMs for motors
+   ledcAttachPin(climbMotorFPin, 1); // F means forward
+   ledcAttachPin(climbMotorRPin, 2); // R means reverse
+   ledcSetup(1, 20000, 8);
+   ledcSetup(2, 20000, 8);
+
+   Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Button: ");
+  Serial.print(button);
+  Serial.print(" Motor: ");
+  Serial.println(motorOn);
+  
+  button = digitalRead(buttonPin);
+  if (button == 1 && buttonLast == 0) // Button has just been pressed
+    motorOn = !motorOn;               // Turn the motor on if off, turn motor off if on
 
+  if (motorOn) {
+    ledcWrite(1, 255);
+    ledcWrite(2, 0);
+  } else {
+    ledcWrite(1, 0);
+    ledcWrite(2, 0);
+  }
+
+  buttonLast = button;
 }
