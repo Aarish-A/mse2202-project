47712d5a7062a06f9f6277055fbb5d56dbf5802c
Aarish Adeel <aarish8@gmail.com>
2021-03-21 18:46:36 -0400

    Add motor speed control using potentiometer 2

--- mse2202-project/mse2202-project.ino
+++ mse2202-project/mse2202-project.ino
@@ -1,5 +1,6 @@
 /* Sensor Pins */
 const int buttonPin = 27;
+const int potiPin = 32;
 
 /* Motor Pins */
 const int climbMotorFPin = 15;
@@ -8,13 +9,18 @@ const int climbMotorRPin = 2;
 /* Sensor State/Time Variables */
 int button = 1;
 int buttonLast = 1;
+int poti = 0;
 
 /* Motor State/Time Variables */
 bool motorOn = false;
+const int minMotorSpeed = 150;
+const int maxMotorSpeed = 255;
+int motorSpeed = 0;
 
 void setup() {
    // Sensor pin setup
    pinMode(buttonPin, INPUT_PULLUP);
+   pinMode(potiPin, INPUT);
   
    // ledc (LED Control) pins setup, used as PWMs for motors
    ledcAttachPin(climbMotorFPin, 1); // F means forward
@@ -26,17 +32,22 @@ void setup() {
 }
 
 void loop() {
+  button = digitalRead(buttonPin);
+  poti = analogRead(potiPin);
+  motorSpeed = map(poti, 0, 4095, minMotorSpeed, maxMotorSpeed);
+  
   Serial.print("Button: ");
   Serial.print(button);
+  Serial.print(" Poti: ");
+  Serial.print(poti);
   Serial.print(" Motor: ");
   Serial.println(motorOn);
   
-  button = digitalRead(buttonPin);
   if (button == 1 && buttonLast == 0) // Button has just been pressed
     motorOn = !motorOn;               // Turn the motor on if off, turn motor off if on
 
   if (motorOn) {
-    ledcWrite(1, 255);
+    ledcWrite(1, motorSpeed);
     ledcWrite(2, 0);
   } else {
     ledcWrite(1, 0);
